3558b9a031260beb837351819845aa73e330cc41
kkhanipov <32584968+kkhanipov@users.noreply.github.com>
2018-02-11 12:17:01 -0600

    stuff 1

--- RPA/Array_Sequences.h
+++ RPA/Array_Sequences.h
@@ -10,9 +10,10 @@ using namespace std;
 class Array_Sequences
 {
 	Sequence ** sequence; //an array of pointers to sequence objects
+	unsigned int * sequence_length;
 	unsigned int max_number_of_sequences; //this is the array size
 	unsigned int number_of_sequences; // number of sequences located inside the array
-
+	unsigned int max_sequence_length;
 
 public:
 	Array_Sequences(char * filename, ostream &err_msg = cout); //constructor to be able to read a FASTA file and create the individual sequences and array of sequences
@@ -25,14 +26,25 @@ public:
 	bool show_All(ostream & out = cout, ostream &err_msg = cout); // shows the basic statistics about the array of sequences and then prints to screen the actual sequences
 
 	bool add_sequence(Sequence * sequence, ostream &err_msg = cout); //ability to add a sequence to the array of sequences by sequence object
-	bool add_sequence(char * sequence, ostream &err_msg = cout); //ability to add a sequence to the array of sequences by giving the actual sequence
+	bool add_sequence(char * sequence, unsigned int _sequence_length, ostream &err_msg = cout); //ability to add a sequence to the array of sequences by giving the actual sequence
 
 	unsigned int get_number_of_sequences(ostream &err_msg = cout);
 	Sequence * get_pointer_to_sequence_object(unsigned int position, ostream &err_msg = cout);
 };
 
+Array_Sequences::Array_Sequences(unsigned int _max_number_of_sequences, ostream &err_msg)
+{
+	number_of_sequences = 0;
+	max_number_of_sequences = _max_number_of_sequences;
+	max_sequence_length = 4000000000;
+}
+
 Array_Sequences::Array_Sequences(char * filename, ostream &err_msg)
 {
+	number_of_sequences = 0;
+	max_number_of_sequences = 5000;
+	max_sequence_length = 4000000000;
+
 	// Check if input file is opened successfully
 	if (filename == NULL)
 	{
@@ -54,8 +66,6 @@ Array_Sequences::Array_Sequences(char * filename, ostream &err_msg)
 		throw ("Failed to read header");
 	}
 
-	unsigned int max_sequence_length = 4000000000;
-
 	char * _sequence = new char[max_sequence_length + 1];
 	if (_sequence == NULL)
 	{
@@ -67,74 +77,49 @@ Array_Sequences::Array_Sequences(char * filename, ostream &err_msg)
 	unsigned int i;
 
 	
-	// Create sequence length array
-	sequence_length = new unsigned int[array_size];
-	if (sequence_length == NULL)
-	{
-		err_msg_out << "ERROR: Array_of_Sequences::read_Array_of_Sequences_From_FASTA_File(...) ==> Not enough memory (sequence_length == NULL)" << endl;
-		in.close(); delete[] _header_text; delete[] _sequence; delete[] copy_number; return false;
-	}
 
-
-	// Create sequences array
-	sequence = new char *[array_size];
-	if (sequence == NULL)
-	{
-		err_msg_out << "ERROR: Array_of_Sequences::read_Array_of_Sequences_From_FASTA_File(...) ==> Not enough memory (sequence == NULL)" << endl;
-		in.close(); delete[] _header_text; delete[] _sequence; delete[] copy_number; delete[] sequence_length; return false;
-	}
 
 	// Initialize 
-	for (i = 0; i<array_size; i++) { copy_number[i] = 0; sequence_length[i] = 0; sequence[i] = NULL; }
+	for (i = 0; i<max_number_of_sequences; i++) {sequence[i] = NULL; }
 
 	unsigned int ii;
-
-	unsigned int position = 0;
 
 	// Read sequences in file
 	while (!in.eof())
 	{
-		in.getline(_header_text, max_text_length);
+		in.getline(_header_text, max_sequence_length);
 		if (_header_text[0] == '\0' || _header_text[0] == EOF) break;
 
 		in.getline(_sequence, max_sequence_length + 1);
 
 		for (i = 0; i<max_sequence_length + 1; i++) if (_sequence[i] == '\0') break;
-		sequence_length[position] = i;
+				
+		sequence[number_of_sequences] = new Sequence(_sequence, i);
+		
 
-		sequence[position] = new char[i + 1];
-		if (sequence[position] == NULL)
+		if (sequence[number_of_sequences] == NULL)
 		{
-			err_msg_out << "ERROR: Array_of_Sequences::read_Array_of_Sequences_From_FASTA_File(...) ==> Not enough memory (sequence[" << i << "] == NULL)" << endl;
-			in.close(); delete[] _header_text; delete[] _sequence;  delete[] copy_number; delete[] sequence_length;
-			for (ii = 0; ii<i; ii++) if (sequence[i] != NULL) delete[] sequence[ii]; // Delete all allocated sequences
-			delete[] sequence; return false;
+			err_msg << "ERROR: Array_Sequences::Array_Sequences(...) ==> Not enough memory (sequence[" << i << "] == NULL)" << endl;
+			in.close(); delete[] _header_text; delete[] _sequence; 
+			for (ii = 0; ii < i; ii++) if (sequence[i] != NULL) {} ; // Delete all allocated sequences
+			delete[] sequence; throw ("Not enough memory to allocate for sequence");
 		}
 
-		sequence[position][i] = '\0';
-
-		// Copy new sequence in the object
-		for (i = 0; i< sequence_length[position]; i++) sequence[position][i] = _sequence[i];
-		sequence[position][sequence_length[position]] = '\0';
-		copy_number[position] = 1;
-		position++;
-
+		
+		number_of_sequences++;
 		// Increase array size if needed
-		if (position == array_size - 1)
+		if (number_of_sequences == max_number_of_sequences - 1)
 		{
-			if (!z_increase_Array_Size())
-			{
-				err_msg_out << "ERROR: Array_of_Sequences::read_Array_of_Sequences_From_FASTA_File(...) ==> z_increase_Array_Size() == false" << endl;
-				in.close(); delete[] _header_text; delete[] _sequence; delete[] copy_number; delete[] sequence_length;
-				for (i = 0; i<array_size; i++) if (sequence[i] != NULL) delete[] sequence[i];	// Delete all allocated sequences
-				delete[] sequence; return false;
-			}
+			//realloc stuff
 		}
 	}
 
 
 	delete[] _header_text;
 	delete[] _sequence;
+}
 
-	return true;
+bool Array_Sequences::add_sequence(Sequence * sequence, unsigned int _sequence_length, ostream &err_msg)
+{
+
 }
--- RPA/Sequence.h
+++ RPA/Sequence.h
@@ -15,11 +15,69 @@ public:
 	bool show_statistics(ostream & out=cout, ostream &err_msg=cout); //prints out the nucleotide contribution of the sequence
 	bool show_All(ostream & out = cout, ostream &err_msg = cout); //prints out the show_statistics() and then the actual sequence
 
-	char * get_pointer_to_sequence() {return sequence; };
+	char * get_pointer_to_sequence() {return sequence;};
 
 };
 
-Sequence::Sequence(char * sequence, unsigned int sequence_length, ostream & err_msg);
+Sequence::Sequence(char * _sequence, unsigned int _sequence_length, ostream & err_msg)
 {
+	if (_sequence_length == 0)
+	{
+		err_msg << "ERROR: Sequence::Sequence ==> _sequence_length == 0" << endl; throw ("Sequence could not be allocated b/c length is 0");
+	}
+	sequence = new char[_sequence_length];
+	seq_length = _sequence_length;
+	if (sequence == NULL)
+	{
+		err_msg << "ERROR: Sequence::Sequence ==> sequence == NULL" << endl; throw ("Sequence could not be allocated");
+	}
+	for (int i = 0; i < _sequence_length; i++)
+	{
+		sequence[i] = _sequence[i];
+	}
+}
 
+bool Sequence::show_statistics(ostream & out, ostream &err_msg)
+{
+	int count_A = 0;
+	int count_C = 0;
+	int count_T = 0;
+	int count_G = 0;
+	int count_N = 0;
+	for (int i = 0; i < seq_length; i++)
+	{
+		switch (sequence[i])
+		{
+		case 'A':case 'a': count_A++; break;
+		case 'T':case 't': count_T++; break;
+		case 'C':case 'c': count_C++; break;
+		case 'G':case 'g': count_G++; break;
+		case 'N':case 'n': count_N++; break;
+		default:
+			err_msg << "ERROR: Sequence::Sequence ==> unexpected character: sequence[" << i << "]=" << sequence[i] << endl;
+			return false;
+		}
+	}
+
+	out << "Nucleotide Contribution is:" << endl;
+	out << "A :"<< count_A << endl;
+	out << "C :" << count_C << endl;
+	out << "T :" << count_T << endl;
+	out << "G :" << count_G << endl;
+	out << "N :" << count_N << endl;
+
+	return true;
+}
+
+bool Sequence::show_All(ostream & out, ostream &err_msg)
+{
+	show_statistics();
+
+	for (int i = 0; i < seq_length; i++)
+	{
+		out << sequence[i];
+	}
+	out << endl;
+
+	return true;
 }
